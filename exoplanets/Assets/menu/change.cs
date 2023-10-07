79f40f081c726acee19cea7823cf3a3b84805a69
wamonser <wamonser@espol.edu.ec>
2023-10-07 13:34:42 -0500

    hello

---
+++ exoplanets/Assets/menu/change.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class change : MonoBehaviour
+{
+    public void cambiarScene(string nombre)
+    {
+    SceneManager.LoadScene(nombre);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
