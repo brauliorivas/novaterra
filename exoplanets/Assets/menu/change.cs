c428de8ff90cc5a1f7ea9bf0e9abc1a8c3ec0ba5
erillope <ericklorenzolopez1@gmail.com>
2023-10-07 20:24:23 -0500

    Escenas funcionables

--- exoplanets/Assets/menu/change.cs
+++ exoplanets/Assets/menu/change.cs
@@ -9,15 +9,4 @@ public class change : MonoBehaviour
     SceneManager.LoadScene(nombre);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
